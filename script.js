a9895fb41b852c070dcaf21fda8a8cb3f3f7c5ed
Suru3458A <stak9964@gmail.com>
2025-05-19 12:38:55 +0530

    Initial commit

--- script.js
+++ script.js
@@ -88,6 +88,13 @@ const questions = [
     }
 
 ];
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+  shuffle(questions);
 
 // Because one question’s correctAnswer is ‘Pacific’ but option has only “City”, "Ocean", etc.
 // We'll fix this: For the first question, change correctAnswer to the correct option, e.g. "Ocean"
